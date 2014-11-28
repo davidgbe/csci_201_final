2c29eb8ca179a4cca0a3c0b465f50785b0e25110
James Perng <jperng@usc.edu>
2014-11-27 17:46:01 -0800

    Attack getters

--- Attack.java
+++ Attack.java
@@ -3,5 +3,22 @@ public class Attack{
 	private int damage;
 	private double accuracy;
 	private double crit;
-	
+	public Attack(String name, int damage, double acc, double crit){
+		this.name = name;
+		this.damage = damage;
+		this.accuracy = acc;
+		this.crit = crit;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public int getDamage(){
+		return this.damage;
+	}
+	public double getAccuracy(){
+		return this.accuracy;
+	}
+	public double getCrit(){
+		return this.crit;
+	}
 }
