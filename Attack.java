d47626d009384b4ef4e865eb9e812828612c0876
James Perng <jperng@usc.edu>
2014-11-27 18:00:25 -0800

    Attack getters

--- Attack.java
+++ Attack.java
@@ -6,19 +6,32 @@ public class Attack{
 	public Attack(String name, int damage, double acc, double crit){
 		this.name = name;
 		this.damage = damage;
-		this.accuracy = acc;
-		this.crit = crit;
+		this.accuracy = acc; // number between 0 and 1;
+		this.crit = crit; // number between 0 and 1;
 	}
 	public String getName(){
 		return this.name;
 	}
-	public int getDamage(){
-		return this.damage;
-	}
-	public double getAccuracy(){
-		return this.accuracy;
-	}
-	public double getCrit(){
-		return this.crit;
+//	public int getDamage(){
+//		return this.damage;
+//	}
+//	public double getAccuracy(){
+//		return this.accuracy;
+//	}
+//	public double getCrit(){
+//		return this.crit;
+//	}
+	public double totalDamage(Pokemon pok){
+		if (Math.random()<this.accuracy){
+			if (Math.random()<this.crit){
+				return 1.5*this.damage*pok.getStrength();
+			}
+			else{
+				return this.damage*pok.getStrength();
+			}
+		}
+		else{
+			return 0.0;
+		}
 	}
 }
